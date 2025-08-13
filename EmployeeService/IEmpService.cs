541b2fd222f2230fe19fe7f0e14482151e8d77bc
farukjnu1@gmail.com <farukjnu1@gmail.com>
2025-08-14 04:32:48 +0800

    Add project files.

---
+++ EmployeeService/IEmpService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmpService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmpService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/EmpsalaryDetail",
+            RequestFormat =WebMessageFormat.Json,
+            ResponseFormat =WebMessageFormat.Json,
+            BodyStyle =WebMessageBodyStyle.Wrapped)]
+      string GetEmpSalary(string Empid);
+    }
+}
